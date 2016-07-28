cc75be597f2e0166bf2a11b998b0f6d3f9b12b2d
MacTHEgenius <levesque.benoit@me.com>
2016-07-28 02:22:25 -0400

    Initial Commit

---
+++ GiftExchange/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GiftExchange
+//
+//  Created by Benoît Lévesque on 2016-07-28.
+//  Copyright © 2016 Benoît Lévesque. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
