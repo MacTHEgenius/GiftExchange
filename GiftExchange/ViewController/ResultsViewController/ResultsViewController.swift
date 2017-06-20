f8d2a1391a704fb9d17a0cf8b3b2237b1747df1c
MacTHEgenius <levesque.benoit@me.com>
2017-06-20 16:27:16 -0400

    Design UX on top view

--- GiftExchange/ViewController/ResultsViewController/ResultsViewController.swift
+++ GiftExchange/ViewController/ResultsViewController/ResultsViewController.swift
@@ -32,9 +32,24 @@ class ResultsViewController: UIViewController {
             self.delegate = ResultsTableViewDelegate(parent: self, tableView: self.resultsTable, participants: controller, results: results)
         }
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.topView.isHidden = false
+        self.topView.alpha = 1.0
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Hide Top view
+    
+    @IBAction func onSeeResultsTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.topView.alpha = 0.0
+        }) { (animated) in
+            self.topView.isHidden = true
+        }
     }
 
 }
