ed6e1504902fc7cca673a726c3085f14e6ffc93c
MacTHEgenius <levesque.benoit@me.com>
2017-07-03 14:53:32 -0400

    Model done for cannot pick condition (little refactor for participant controller

--- Swap/Controller/ParticipantController.swift
+++ Swap/Controller/ParticipantController.swift
@@ -13,29 +13,64 @@ class ParticipantController {
     let parent: ParticipantsController
     let participant: Participant?
     
-    private(set) var cantPick: [String:Bool] = [:]
+    private(set) var cantPick: [String] = []
     
     init(_ participant: Participant? = nil, parent: ParticipantsController) {
         self.participant = participant
         self.parent = parent
         if let participant = self.participant {
-            self.cantPick = participant.cantPick.reduce(self.cantPick) { (dict, participantId) -> [String:Bool] in
-                var d = dict
-                d[participantId] = true
-                return d
-            }
+            self.cantPick = participant.cantPick
         }
     }
     
-    func doNotPick(_ participant: Participant, isPicked: Bool) {
-        if let current = self.participant, current.id != participant.id {
-            self.cantPick[participant.id] = isPicked
+    // Replacement of doNotPickParticipant:isPicked
+    func toggle(_ participant: Participant) {
+        let count = self.parent.count
+        let cantPickCount = self.cantPick.count + 1
+        
+        if let current = self.participant {
+            
+            if current.id == participant.id {
+                print("Cannot toggle self participant") // Alert ?
+            } else {
+                if self.cantPick.contains(participant.id) {
+                    self.cantPick.remove(at: self.cantPick.index(of: participant.id)!)
+                } else {
+                    if count - (cantPickCount + 1) >= 2 {
+                        self.cantPick.append(participant.id)
+                    } else {
+                        print("not enough")
+                    }
+                }
+            }
+            
         }
     }
+    
+    /*func doNotPick(_ participant: Participant, isPicked: Bool) {
+        if let current = self.participant {
+            print(self.parent.count, self.cantPick.count)
+            
+            if current.id == participant.id {
+                // error...
+            } else {
+                if self.parent.count - self.cantPick.count >= 2 {
+                    self.cantPick[participant.id] = isPicked
+                } else {
+                    print("not enough")
+                }
+            }
+            
+//            if self.parent.count - self.cantPick.count < 2 {
+//                print(self.parent.count, self.cantPick.count, "not enough")
+//            } else if current.id != participant.id {
+//                self.cantPick[participant.id] = isPicked
+//            }
+        }
+    }*/
     
     func save(_ participant: Participant) throws {
-        let temp = self.cantPick.map({ (key, value) in value ? key : "" })
-        participant.cantPick = temp.filter({ (str) in str != "" })
+        participant.cantPick = self.cantPick
         
         if participant.valid {
             if let p = self.participant {
@@ -51,10 +86,12 @@ class ParticipantController {
 
 enum ParticipantError: Error, Equatable {
     case notValid(errors: [String])
+    case notEnoughPicked(count: Int)
 }
 
 func ==(lhs: ParticipantError, rhs: ParticipantError) -> Bool {
     switch (lhs, rhs) {
     case (.notValid(let leftErrors), .notValid(let rightErrors)): return leftErrors == rightErrors
+    default: return true
     }
 }
